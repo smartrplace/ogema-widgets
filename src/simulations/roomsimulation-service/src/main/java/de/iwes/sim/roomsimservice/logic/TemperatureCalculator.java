2a0110daa3b99eb29c6ca28bf73da4da4f5b8f8e
David Nestle <david.nestle@iee.fraunhofer.de>
2020-07-04 18:41:30 +0200

    minor fix room simulation

--- src/simulations/roomsimulation-service/src/main/java/de/iwes/sim/roomsimservice/logic/TemperatureCalculator.java
+++ src/simulations/roomsimulation-service/src/main/java/de/iwes/sim/roomsimservice/logic/TemperatureCalculator.java
@@ -96,6 +96,10 @@ public class TemperatureCalculator {
 		if (kelvinAdded < 0 && newValue < outsideTemperature) {	// should not happen, but could be the case if updateInterval is chosen to big
 			newValue = outsideTemperature;   
 		}
+		if(newValue < 280.15f)
+			newValue = 280.15f;
+		else if(newValue > 313.15f)
+			newValue = 313.15f;
 		//logger.debug("  Calculated new temperature value. time diff "  + (currentTime - lastUpdateTime)/1000 + "s, current temp : " + currentTemperature + ", outsideTemp: " + outsideTemperature + 
 		//		", radiator energy input: " + energyAdded + ", energy loss: " + energyLoss + ", Kelvins added: " + kelvinAdded );
 		return newValue; 
