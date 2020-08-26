a00fdefc04ab4e0aeec34057a763bb44f7c11e21
David Nestle <david.nestle@iee.fraunhofer.de>
2020-08-26 16:17:05 +0200

    add example UserLocaleUtil

---
+++ src/ogema-js-bundle/src/main/java/de/iwes/widgets/api/extended/impl/UserLocaleUtil.java
@@ -0,0 +1,21 @@
+package de.iwes.widgets.api.extended.impl;
+
+import org.ogema.core.administration.UserAccount;
+import org.ogema.core.administration.UserConstants;
+import org.ogema.core.application.ApplicationManager;
+
+public class UserLocaleUtil {
+	public static String getLocaleString(String userName, ApplicationManager appMan) {
+		UserAccount userAccount = appMan.getAdministrationManager().getUser(userName);
+		if(userAccount == null)
+			return null;
+		return userAccount.getProperties().getOrDefault(UserConstants.PREFERRED_LOCALE, "EN").toString();
+	}
+	
+	public static void setLocaleString(String userName, String localeString, ApplicationManager appMan) {
+		UserAccount userAccount = appMan.getAdministrationManager().getUser(userName);
+		if(userAccount == null)
+			return;
+		userAccount.getProperties().put(UserConstants.PREFERRED_LOCALE, localeString);
+	}
+}
