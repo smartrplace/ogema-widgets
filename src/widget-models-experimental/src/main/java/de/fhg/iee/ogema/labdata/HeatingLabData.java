f43a9ae11a41fd70254c1af1dfec6c417b1ade73
David Nestle <david.nestle@iee.fraunhofer.de>
2021-02-24 23:53:05 +0100

    added HeatingLabData model

---
+++ src/widget-models-experimental/src/main/java/de/fhg/iee/ogema/labdata/HeatingLabData.java
@@ -0,0 +1,16 @@
+package de.fhg.iee.ogema.labdata;
+
+import org.ogema.core.model.ResourceList;
+import org.ogema.model.devices.connectiondevices.ThermalValve;
+import org.ogema.model.prototypes.PhysicalElement;
+import org.ogema.model.sensors.TemperatureSensor;
+
+public interface HeatingLabData extends PhysicalElement {
+	/** put MR1_LT, MR2_LT, MR3_LT here*/
+	ResourceList<TemperatureSensor> airTemperature();
+	
+	/** Put HK_VL in ThermalValve.connection.inputTemperature and
+	 * HK_RL in outputTemperature for each radiator
+	 */
+	ResourceList<ThermalValve> radiators();
+}
