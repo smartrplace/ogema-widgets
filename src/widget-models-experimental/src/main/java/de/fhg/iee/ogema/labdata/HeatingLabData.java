6ee0a80e47327432d78eb92124944e333dee9e41
David Nestle <david.nestle@iee.fraunhofer.de>
2021-11-02 09:24:03 +0100

    HeatingLabData update

--- src/widget-models-experimental/src/main/java/de/fhg/iee/ogema/labdata/HeatingLabData.java
+++ src/widget-models-experimental/src/main/java/de/fhg/iee/ogema/labdata/HeatingLabData.java
@@ -1,8 +1,10 @@
 package de.fhg.iee.ogema.labdata;
 
 import org.ogema.core.model.ResourceList;
+import org.ogema.model.connections.ThermalConnection;
 import org.ogema.model.devices.connectiondevices.ThermalValve;
 import org.ogema.model.prototypes.PhysicalElement;
+import org.ogema.model.sensors.FlowSensor;
 import org.ogema.model.sensors.TemperatureSensor;
 
 public interface HeatingLabData extends PhysicalElement {
@@ -13,4 +15,12 @@ public interface HeatingLabData extends PhysicalElement {
 	 * HK_RL in outputTemperature for each radiator
 	 */
 	ResourceList<ThermalValve> radiators();
+	
+	/** Put MR1_V, MR2_V, MR3_V here*/
+	ResourceList<FlowSensor> heatFlow();
+	
+	TemperatureSensor outSideTemperature();
+	
+	/** Put G_VL, G_RL here*/
+	ThermalConnection mainConnection();
 }
