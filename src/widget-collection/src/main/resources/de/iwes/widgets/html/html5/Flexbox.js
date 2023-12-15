cb7e0ac570324fd57648d1cb31a2372bc811e45a
cnoelle <christoph.noelle@smartrplace.de>
2023-12-15 23:17:33 +0100

    widget Flexbox: avoid destroying subwidgets when nothing changed

--- src/widget-collection/src/main/resources/de/iwes/widgets/html/html5/Flexbox.js
+++ src/widget-collection/src/main/resources/de/iwes/widgets/html/html5/Flexbox.js
@@ -5,16 +5,29 @@ Flexbox.prototype.constructor = Flexbox;
 function Flexbox(servletPath, widgetID) {
 
     GenericWidget.call(this, servletPath, widgetID);
+    this.lastData = undefined;
     this.sendGET();
 }
 
 /*
  * TODO this is implemented in a very inefficient way: destroy all widgets, then recreate them -> to be improved 
+ * First step: if items did not change at all then we do not recreate them
  */
 Flexbox.prototype.update = function (data, oldRequest, subwidgetsToBeRemoved) {
-    
-	var el = $(this.element).find(">div"); 
-	
+        var isUnchanged = false;     
+        if (this.lastData !== undefined && data.items && this.lastData.items && data.items.length === this.lastData.items.length) {
+        	isUnchanged = true;
+		for (var i=0; i < data.items.length; i++) {
+			if (data.items[i] !== this.lastData.items[i]) {
+				isUnchanged = false;
+				break;
+			}
+		}
+        }    
+        this.lastData = data;
+        if (isUnchanged)
+        	return false;
+	var el = $(this.element).find(">div"); 	
 	var oldSubwidgets = el.find(".ogema-widget"); // should be fine... finds even recursive subwidgets
 	for (var i=0; i<oldSubwidgets.length; i++) {
 		var subwidget = oldSubwidgets.get(i);
@@ -29,7 +42,6 @@ Flexbox.prototype.update = function (data, oldRequest, subwidgetsToBeRemoved) {
 	}
 	
 	el.html(str);
-	
 	return true;
 };
 
