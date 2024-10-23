4f356441e45f7f0a5c9e9cd0f3a78b8abb8030ef
dnestle <dnestle@172.31.128.1>
2024-10-23 14:47:10 +0200

    move org.ogema.model.gateway to wi-models-experimental

--- src/widget-models-experimental/src/main/java/org/ogema/model/gateway/CustomerMessageData.java
+++ src/widget-models-experimental/src/main/java/org/ogema/model/gateway/CustomerMessageData.java
@@ -1,8 +1,10 @@
 package org.ogema.model.gateway;
 
+import org.ogema.core.model.ResourceList;
 import org.ogema.core.model.array.StringArrayResource;
 import org.ogema.core.model.simple.IntegerResource;
 import org.ogema.core.model.simple.StringResource;
+import org.ogema.model.extended.alarming.AlarmGroupData;
 import org.ogema.model.prototypes.Data;
 
 public interface CustomerMessageData extends Data {
@@ -16,4 +18,6 @@ public interface CustomerMessageData extends Data {
 	IntegerResource sendCounter();
 	/** Comment on success of sending etc. */
 	StringResource sendComment();
+	
+	ResourceList<AlarmGroupData> source();
 }
