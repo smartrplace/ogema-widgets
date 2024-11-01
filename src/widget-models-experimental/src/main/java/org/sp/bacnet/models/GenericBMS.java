bd2c02ca9c5e854ec1833a35b96d9c1d840b13bc
dnestle <dnestle@172.31.128.1>
2024-11-01 15:51:53 +0100

    add GenericBMS identifier model

---
+++ src/widget-models-experimental/src/main/java/org/sp/bacnet/models/GenericBMS.java
@@ -0,0 +1,8 @@
+package org.sp.bacnet.models;
+
+import org.ogema.model.prototypes.PhysicalElement;
+
+/** Identification for Building Management System (GLT)*/
+public interface GenericBMS extends PhysicalElement {
+
+}
